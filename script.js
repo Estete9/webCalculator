2096bdb7e54a6b0b958a243c73006edf12836270
Esteban <cepalacios@outlook.com>
2022-06-02 07:43:55 -0500

    make grid of buttons with flexbox

---
+++ script.js
@@ -0,0 +1,9 @@
+// take input from user with buttons
+//check if edit text is not empty
+// take value from edit text
+// convert input to variables
+// differentiate between number and symbol
+// Make list of operation symbols
+// check if input is number, symbol, or other
+// check which operation is being done
+//
