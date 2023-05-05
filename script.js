0167590f4cfef58cc87cfc344e060e9f3b215360
Esteban Palacios <cepalacios@outlook.com>
2023-05-05 00:12:52 -0500

    implement event listener for the numbers

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
-// take input from user with buttons
+// take input from user with buttons with an event listener
+const buttons = Array.from(document.querySelectorAll(".button"));
+let inputText = document.querySelector("#input");
+const nonNumbers = ["C", "()", "%", "+/-", "="];
+
+buttons.forEach(function (button) {
+  button.addEventListener("click", () => {
+    console.log(button.textContent)
+    // add the value of the button to the edit text
+    if (!nonNumbers.includes(button.textContent)) {
+      if(inputText.textContent == "0") {
+        inputText.textContent = ""
+      }
+      inputText.textContent += button.textContent
+    }
+  });
+});
 //check if edit text is not empty
 // take value from edit text
 // convert input to variables
