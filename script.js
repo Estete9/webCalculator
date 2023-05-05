10a62aa1ea9f161720d2f4a3005e0041bb25399b
estete9 <cepalacios@outlook.com>
2023-05-05 10:11:42 -0500

    Add number and operation to input text

--- script.js
+++ script.js
@@ -2,17 +2,14 @@
 const buttons = Array.from(document.querySelectorAll(".button"));
 let inputText = document.querySelector("#input");
 const nonNumbers = ["C", "()", "%", "+/-", "="];
+const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const operators = ["*", "/", "+", "-"];
 
 buttons.forEach(function (button) {
   button.addEventListener("click", () => {
-    console.log(button.textContent)
+    console.log(button.textContent);
     // add the value of the button to the edit text
-    if (!nonNumbers.includes(button.textContent)) {
-      if(inputText.textContent == "0") {
-        inputText.textContent = ""
-      }
-      inputText.textContent += button.textContent
-    }
+    buttonCLicked(button);
   });
 });
 //check if edit text is not empty
@@ -23,3 +20,39 @@ buttons.forEach(function (button) {
 // check if input is number, symbol, or other
 // check which operation is being done
 //
+
+function buttonCLicked(button) {
+  // checks if it is a number
+  if (numbers.includes(button.textContent)) {
+    if (inputText.textContent == "0") {
+      inputText.textContent = "";
+    }
+    addNumberToInputText(button);
+  }
+
+  // check if it an operation
+  if (operators.includes(button.textContent)) {
+    if (!inputText.textContent == "0") {
+      addOperatorToInputText(button);
+    }
+  }
+}
+
+function addNumberToInputText(button) {
+  inputText.textContent += button.textContent;
+}
+
+function addOperatorToInputText(button) {
+  if (
+    operators.includes(
+      inputText.textContent.charAt(inputText.textContent.length - 1)
+    )
+  ) {
+    let newString =
+      inputText.textContent.substring(0, inputText.textContent.length - 1) +
+      button.textContent;
+    inputText.textContent = newString;
+  } else {
+    inputText.textContent += button.textContent;
+  }
+}
